675bd114556b99df3bb07e8a2e2cc8300711b08e
Lauren Stephenson <compscilauren@gmail.com>
2020-03-18 14:41:21 -0500

    Add basic table to home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,174 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TableFilter from 'tablefilter';
 
 function App() {
+  // var tf = new TableFilter(document.querySelector('.my-table'), {
+  //   base_path: 'path/to/my/scripts/tablefilter/',
+  // });
+  // tf.init();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Stardew Valley Server Finder</p>
       </header>
+      <body>
+        <table className="my-table">
+          <tr>
+            <th>User</th>
+            <th>Status</th>
+            <th>Timezone</th>
+            <th>Players Needed</th>
+            <th>Platform</th>
+            <th>Mods</th>
+            <th>Invite Code</th>
+            <th>Other</th>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>3</td>
+            <td>XBox</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +6</td>
+            <td>3</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>2</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>3</td>
+            <td>PC or Switch</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT+8</td>
+            <td>2</td>
+            <td>XBox</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>2</td>
+            <td>Switch</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +8</td>
+            <td>1</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +6</td>
+            <td>3</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>2</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+          <tr>
+            <td>CompSciLauren</td>
+            <td>Hosting</td>
+            <td>GMT +4</td>
+            <td>1</td>
+            <td>PC</td>
+            <td>None</td>
+            <td>DM me</td>
+            <td>
+              I can vc, I have experience and speak fluent english. Active
+              daily. Timezone must be close to mine. I'd love to join people who
+              are active and can vc, thanks.
+            </td>
+          </tr>
+        </table>
+      </body>
     </div>
   );
 }
