257c88b29b19be2f5a13c30e0d949ab68dedb280
Lauren Stephenson <compscilauren@gmail.com>
2020-05-06 13:56:51 -0500

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, {Component} from 'react';
-import ReactDom from 'react-dom';
+import React, { Component } from 'react';
 import './App.css';
 import SampleData from './data.json';
 import TableFilter from 'react-table-filter';
@@ -7,70 +6,53 @@ import {} from './styles.css';
 import {} from './example.scss';
 
 class App extends Component {
-  // var tf = new TableFilter(document.querySelector('.my-table'), {
-  //   base_path: 'path/to/my/scripts/tablefilter/',
-  // });
-  // tf.init();
-
   constructor(props) {
     super(props);
     this.state = {
-      'data': SampleData.test.data,
+      data: SampleData.test.data,
     };
     this._filterUpdated = this._filterUpdated.bind(this);
   }
 
   _filterUpdated(newData, filtersObject) {
     this.setState({
-      'data' : newData,
+      data: newData,
     });
   }
 
-    render(){
-      const data = this.state.data;
-      const elementsHtml = data.map((item, index) => {
-        return (
-          <tr key={'row_'+index}>
-            <td className="cell">
-              {item.user}
-            </td>
-            <td className="cell">
-              {item.status}
-            </td>
-            <td className="cell">
-              {item.timezone}
-            </td>
-            <td className="cell">
-              {item.playersneeded}
-            </td>
-            <td className="cell">
-              {item.platform}
-            </td>
-            <td className="cell">
-              {item.mods}
-            </td>
-            <td className="cell">
-              {item.invitecode}
-            </td>
-            <td className="cell">
-              {item.other}
-            </td>
-          </tr>
-        );
-      });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="main_logo.png" className="App-logo" alt="logo" />
-        <p>Stardew Valley Server Finder</p>
-      </header>
+  render() {
+    const data = this.state.data;
+    const elementsHtml = data.map((item, index) => {
+      return (
+        <tr key={'row_' + index}>
+          <td className="cell">{item.user}</td>
+          <td className="cell">{item.status}</td>
+          <td className="cell">{item.timezone}</td>
+          <td className="cell">{item.playersneeded}</td>
+          <td className="cell">{item.platform}</td>
+          <td className="cell">{item.mods}</td>
+          <td className="cell">{item.invitecode}</td>
+          <td className="cell">{item.other}</td>
+        </tr>
+      );
+    });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="main_logo.png" className="App-logo" alt="logo" />
+          <p>Stardew Valley Server Finder</p>
+        </header>
 
-      <table>
-        <thread>
-          <TableFilter 
-            rows={data}
-            onFilterUpdate={this._filterUpdated}>
-              <th key="user" filterkey="user" className="cell" casesensitive={'true'} showsearch={'true'}>
+        <table>
+          <thread>
+            <TableFilter rows={data} onFilterUpdate={this._filterUpdated}>
+              <th
+                key="user"
+                filterkey="user"
+                className="cell"
+                casesensitive={'true'}
+                showsearch={'true'}
+              >
                 User
               </th>
               <th key="status" filterkey="status" className="cell">
@@ -79,31 +61,34 @@ class App extends Component {
               <th key="timezone" filterkey="timezone" className="cell">
                 Timezone
               </th>
-              <th key="playersneeded" filterkey="playersneeded" className="cell">
+              <th
+                key="playersneeded"
+                filterkey="playersneeded"
+                className="cell"
+              >
                 Players Needed
               </th>
               <th key="platform" filterkey="platform" className="cell">
                 Platform
               </th>
-              <th key="mods" filterkey="mods" className="cell" casesensitive={'true'} showsearch={'true'}>
+              <th
+                key="mods"
+                filterkey="mods"
+                className="cell"
+                casesensitive={'true'}
+                showsearch={'true'}
+              >
                 Mods
               </th>
-              <th>
-                Invite Code
-              </th>
-              <th>
-                Other
-              </th>
-          </TableFilter>
-        </thread>
-        <tbody>
-          { elementsHtml }
-        </tbody>
-      </table>
-    </div>
-  );
- }
+              <th>Invite Code</th>
+              <th>Other</th>
+            </TableFilter>
+          </thread>
+          <tbody>{elementsHtml}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
 
 export default App;
