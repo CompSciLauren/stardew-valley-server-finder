287f517f84b659e155ed4b8072e850fc87e70e83
Lauren Stephenson <compscilauren@gmail.com>
2020-05-14 16:07:11 -0500

    Add basic text

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,8 +3,27 @@ import React from 'react';
 const Profile = () => {
   return (
     <div>
-      <p>Username: CompSciLauren</p>
-      <p>My Info on Home Page:</p>
+      <div>
+        <p>Your Public Information:</p>
+        <p>Username: CompSciLauren</p>
+        <p>Status:</p>
+        <p>Timezone:</p>
+        <p>Players Needed:</p>
+        <p>Platform:</p>
+        <p>Mods:</p>
+        <p>Invite Code: DM me</p>
+        <p>Notes:</p>
+      </div>
+      <div>
+        <p>Your Private Information:</p>
+        <p>Invite Code:</p>
+      </div>
+      <div>
+        <p>Options:</p>
+        <p>Change Username</p>
+        <p>Change Password</p>
+        <p>Delete Account</p>
+      </div>
     </div>
   );
 };
