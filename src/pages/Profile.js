46dfc4030ab5757e0334ea86fab6c9bc607830f4
Lauren Stephenson <compscilauren@gmail.com>
2020-05-14 18:13:39 -0500

    Add CSS grids

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -20,50 +20,90 @@ const Profile = () => {
     <div>
       <div>
         <h1>My Public Information</h1>
-        <label>Username</label>
-        <p>{playerInfo[0].username}</p>
-        <label>Status</label>
-        <p>
-          {' '}
-          {playerInfo[0].playerStatuses.map((status) => (
-            <p key={playerInfo[0].username + ':' + status}>{status}</p>
-          ))}
-        </p>
-        <label>Timezone</label>
-        <p>{playerInfo[0].timezone}</p>
-        <label>Players Needed</label>
-        <p>{playerInfo[0].num_players}</p>
-        <label>Platform</label>
-        <p>{playerInfo[0].platform}</p>
-        <label>Mods</label>
-        <p>{playerInfo[0].mods}</p>
-        <label>Invite Code</label>
-        <input type="username" id="username" value="AJF72H" />
-        <label>Set to public</label>
-        <input type="checkbox" id="inviteCode" name="inviteCode"></input>
-        <label>Notes</label>
-        <input
-          type="text"
-          id="notes"
-          class="largeInputField"
-          value={playerInfo[0].notes}
-        ></input>
+        <div class="grid-container">
+          <div class="grid-item">
+            <label>Username</label>
+            <p>{playerInfo[0].username}</p>
+          </div>
+          <div class="grid-item">
+            <label>Status</label>
+            <p>
+              {' '}
+              {playerInfo[0].playerStatuses.map((status) => (
+                <p key={playerInfo[0].username + ':' + status}>
+                  {status}
+                  <input
+                    type="checkbox"
+                    id="statusCheckbox"
+                    name="statusCheckbox"
+                  ></input>
+                </p>
+              ))}
+            </p>
+          </div>
+          <div class="grid-item">
+            <label>Timezone</label>
+            <p>{playerInfo[0].timezone}</p>
+          </div>
+          <div class="grid-item">
+            <label>Players Needed</label>
+            <p>{playerInfo[0].num_players}</p>
+          </div>
+          <div class="grid-item">
+            <label>Platform</label>
+            <p>{playerInfo[0].platform}</p>
+          </div>
+          <div class="grid-item">
+            <label>Mods</label>
+            <p>{playerInfo[0].mods}</p>
+          </div>
+
+          <div class="grid-item">
+            <label>Invite Code</label>
+            <input type="username" id="username" value="AJF72H" />
+            <input type="checkbox" id="inviteCode" name="inviteCode"></input>
+            Public
+          </div>
+          <div class="grid-item">
+            <label>Notes</label>
+            <input
+              type="text"
+              id="notes"
+              class="largeInputField"
+              value={playerInfo[0].notes}
+            ></input>
+          </div>
+        </div>
       </div>
 
-      <h1>Options</h1>
-      <h2>Change Username</h2>
-      <label>New Username</label>
-      <input type="username" id="username" value={playerInfo[0].username} />
-      <h2>Change Password</h2>
-      <label>Old Password</label>
-      <input type="oldPassword" id="oldPassword" />
-      <label>New Password</label>
-      <input type="newPassword" id="newPassword" />
-      <label>Confirm New Password</label>
-      <input type="confirmNewPassword" id="confirmNewPassword" />
-      <button class="primaryBtn">Save Changes</button>
-      <button class="primaryBtn">Logout</button>
-      <button class="dangerBtn">Delete Account</button>
+      <h1>Change Username</h1>
+      <div class="grid-container">
+        <div class="grid-item">
+          <label>New Username</label>
+          <input type="username" id="username" value={playerInfo[0].username} />
+        </div>
+      </div>
+
+      <h1>Change Password</h1>
+      <div class="grid-container">
+        <div class="grid-item">
+          <label>Old Password</label>
+          <input type="oldPassword" id="oldPassword" />
+        </div>
+        <div class="grid-item">
+          <label>New Password</label>
+          <input type="newPassword" id="newPassword" />
+        </div>
+        <div class="grid-item">
+          <label>Confirm New Password</label>
+          <input type="confirmNewPassword" id="confirmNewPassword" />
+        </div>
+        <div class="grid-item">
+          <button class="primaryBtn">Save Changes</button>
+          <button class="primaryBtn">Logout</button>
+          <button class="dangerBtn">Delete Account</button>
+        </div>
+      </div>
     </div>
   );
 };
