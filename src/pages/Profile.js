e42f95bcf5eea3bcbba2b60e1ef3e053918b3b3d
Lauren Stephenson <compscilauren@gmail.com>
2020-05-14 16:25:23 -0500

    Add player data to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import useService from '../hooks/useService';
 
 const Profile = () => {
+  const { isLoading, data, setData } = useService();
+
+  if (isLoading || !data) {
+    return <h1>Loading...</h1>;
+  }
+
+  const playerInfo = [];
+  data.map((data) => {
+    if (data.username === 'CompSciLauren') {
+      playerInfo.push(data);
+    }
+  });
+
   return (
     <div>
       <div>
         <p>Your Public Information:</p>
-        <p>Username: CompSciLauren</p>
-        <p>Status:</p>
-        <p>Timezone:</p>
-        <p>Players Needed:</p>
-        <p>Platform:</p>
-        <p>Mods:</p>
+        <p>Username: {playerInfo[0].username}</p>
+        <p>
+          Status:{' '}
+          {playerInfo[0].playerStatuses.map((status) => (
+            <p key={playerInfo[0].username + ':' + status}>{status}</p>
+          ))}
+        </p>
+        <p>Timezone: {playerInfo[0].timezone}</p>
+        <p>Players Needed: {playerInfo[0].num_players}</p>
+        <p>Platform: {playerInfo[0].platform}</p>
+        <p>Mods: {playerInfo[0].mods}</p>
         <p>Invite Code: DM me</p>
-        <p>Notes:</p>
+        <p>Notes: {playerInfo[0].notes}</p>
       </div>
       <div>
         <p>Your Private Information:</p>
